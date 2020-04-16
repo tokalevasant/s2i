141ada49d4b134d38730217d7b892d5eabd68c47
vtokale <vasant.tokale@gmail.com>
2020-04-16 18:58:53 -0500

    wip

--- go-team-docker/src/main/java/com/rh/goteam/MainController.java
+++ go-team-docker/src/main/java/com/rh/goteam/MainController.java
@@ -8,7 +8,7 @@ public class MainController {
 
     @GetMapping(value = {"/"})
     public String greet() {
-        return "And this is an example of a simple Java REST API created using S2I with Docker file\n";
+        return "And this is an example of a simple REST API created using S2I with Docker file\n";
     }
 
 }
