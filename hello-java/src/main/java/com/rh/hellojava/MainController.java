c852ed4290f73c7e20a66900fd30bc06573ab1dc
tokalevasant <3129329+tokalevasant@users.noreply.github.com>
2020-04-16 18:59:20 -0500

    Update MainController.java

--- hello-java/src/main/java/com/rh/hellojava/MainController.java
+++ hello-java/src/main/java/com/rh/hellojava/MainController.java
@@ -9,7 +9,7 @@ public class MainController {
     @GetMapping(value = {"/"})
     public String greet() {
 
-        return "Hello Everyone. This is an example of a simple Java REST API created using S2I with NO Docker file";
+        return "Hello Everyone. This is an example of a simple REST API created using S2I with NO Docker file";
     }
 
 }
